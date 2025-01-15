a82e11ffd0f2e0d3aabfb7d1b4119c7ec0f8326d
Farmonbekfarxodov <farmonfarxodovbek@gmail.com>
2025-01-16 03:02:39 +0500

    translate admin panel

--- app_blogs/admin.py
+++ app_blogs/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from .models import (
+    BlogCategoryModel,
+    BlogTagModel,
+    BlogAuthorModel,
+    BlogModel,
+    BlogCommentModel,
+)
 
-# Register your models here.
+# Register BlogCategoryModel
+@admin.register(BlogCategoryModel)
+class BlogCategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'parent')
+    search_fields = ('title',)
+    list_filter = ('parent',)
+
+# Register BlogTagModel
+@admin.register(BlogTagModel)
+class BlogTagAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    search_fields = ('title',)
+
+# Register BlogAuthorModel
+@admin.register(BlogAuthorModel)
+class BlogAuthorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'get_full_name', 'avatar')
+    search_fields = ('first_name', 'last_name')
+
+    def get_full_name(self, obj):
+        return obj.get_full_name()
+    get_full_name.short_description = "Full Name"
+
+# Register BlogModel
+@admin.register(BlogModel)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'image', 'description')
+    search_fields = ('title',)
+    list_filter = ('categories', 'author')
+
+# Register BlogCommentModel
+@admin.register(BlogCommentModel)
+class BlogCommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'comment', 'user')
+    search_fields = ('comment', 'user__username')
--- furnitica/urls.py
+++ furnitica/urls.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
 from django.conf import settings
 
-urlpatterns = [
+urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
-    path('',include('pages.urls',namespace='pages'))
-]
-
+    path('', include('pages.urls', namespace='pages')),
+)
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL,document_root = settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
