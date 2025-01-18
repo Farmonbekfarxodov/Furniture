ec52d4ed95c54af9fb5940199b4a35f17a144c16
Farmonbekfarxodov <farmonfarxodovbek@gmail.com>
2025-01-18 06:21:17 +0500

    change

--- app_blogs/admin.py
+++ app_blogs/admin.py
@@ -7,20 +7,20 @@ from .models import (
     BlogCommentModel,
 )
 
-# Register BlogCategoryModel
+
 @admin.register(BlogCategoryModel)
 class BlogCategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'parent')
     search_fields = ('title',)
     list_filter = ('parent',)
 
-# Register BlogTagModel
+
 @admin.register(BlogTagModel)
 class BlogTagAdmin(admin.ModelAdmin):
     list_display = ('id', 'title')
     search_fields = ('title',)
 
-# Register BlogAuthorModel
+
 @admin.register(BlogAuthorModel)
 class BlogAuthorAdmin(admin.ModelAdmin):
     list_display = ('id', 'get_full_name', 'avatar')
@@ -30,14 +30,12 @@ class BlogAuthorAdmin(admin.ModelAdmin):
         return obj.get_full_name()
     get_full_name.short_description = "Full Name"
 
-# Register BlogModel
 @admin.register(BlogModel)
 class BlogAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'image', 'description')
     search_fields = ('title',)
     list_filter = ('categories', 'author')
 
-# Register BlogCommentModel
 @admin.register(BlogCommentModel)
 class BlogCommentAdmin(admin.ModelAdmin):
     list_display = ('id', 'comment', 'user')
